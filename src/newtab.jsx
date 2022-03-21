c3ba4333383998d5e6e88b80651b3f202ecf1f12
Rafael Goncalves <capitao.rafa@gmail.com>
2022-03-21 19:49:10 -0300

    Trying to use toggl-api

--- src/newtab.jsx
+++ src/newtab.jsx
@@ -1,6 +1,24 @@
 import { render, h } from 'preact';
+import { useState, useEffect } from 'preact/hooks';
+import TogglClient from 'toggl-api';
 
-render(
-    <div>newTab</div>,
+function NewPage() {
+    const [tokenValue, setTokenValue]=useState('');
+
+
+    useEffect(() => {
+        chrome.storage.sync.get(['token'], (result) => {
+            setTokenValue(result.token)
+            console.log('test', TogglClient)
+
+        })
+    }, [])
+
+    return <div>
+        <p>{tokenValue}</p>
+    </div>
+}
+
+render(<NewPage />,
     document.getElementById('app')
 );
