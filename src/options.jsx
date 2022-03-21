a259bd2fcdf39bea6bd3dc8cded6964ea4ef0c2b
Rafael Goncalves <capitao.rafa@gmail.com>
2022-03-21 11:45:19 -0300

    Testing hooks and inputs

--- src/options.jsx
+++ src/options.jsx
@@ -1,6 +1,24 @@
 import { render, h } from 'preact';
+import { useState } from 'preact/hooks';
 
-render(
-    <div>Options</div>,
+function OptionsPage(props) {
+
+    const [value, setValue]=useState('');
+
+    const onSubmit=(event) => {
+        alert("Submitted a form "+value);
+        event.preventDefault();
+    }
+
+    const onInput=(event) => setValue(event.target.value)
+
+    return <form onSubmit={onSubmit}>
+        <label for="username">Enter API key:</label>
+        <input type="text" value={value} onInput={onInput} />
+        <button type="submit">Submit</button>
+    </form>
+}
+
+render(<OptionsPage />,
     document.getElementById('app')
 );
