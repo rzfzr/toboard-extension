fb96b9e81c4c595f667941c8997355843b5cd69e
Rafael Goncalves <capitao.rafa@gmail.com>
2022-03-21 11:53:25 -0300

    Watching changes

--- webpack.config.js
+++ webpack.config.js
@@ -46,4 +46,10 @@ module.exports = {
             filename: 'options.html'
         })
     ],
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 200,
+        poll: 1000,
+        ignored: './node_modules/',
+    },
 };
