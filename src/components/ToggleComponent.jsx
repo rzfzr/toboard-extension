2139b54adec3b698286f5dde19ab56beee39974e
Rafael Goncalves <capitao.rafa@gmail.com>
2022-03-22 12:20:56 -0300

    Icons

--- src/components/ToggleComponent.jsx
+++ src/components/ToggleComponent.jsx
@@ -1,15 +1,13 @@
 import { render, h } from 'preact';
 
 import Button from '@mui/material/Button';
-// import PlayArrowIcon from '@mui/icons/PlayArrow';
-
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 export default function ToggleComponent(props) {
     return (
-        <div>
-            {/* <PlayArrowIcon /> */}
-            <Button variant="contained" color="primary">
-                {props.entry.description}
-            </Button>
-        </div>
+        <Button
+            size="large"
+            startIcon={props.entry.isRunning? <PauseCircleFilledIcon fontSize="large" />:<PlayArrowIcon fontSize="large" />}
+            variant="contained" color="primary" >{props.entry.description}
+        </Button>
     );
 }
