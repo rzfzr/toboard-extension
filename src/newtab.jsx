9b93f6f3893811470e8cae11a432a6a2f5cf358b
Rafael Goncalves <capitao.rafa@gmail.com>
2022-03-22 07:06:59 -0300

    Getting entries

--- src/newtab.jsx
+++ src/newtab.jsx
@@ -1,10 +1,10 @@
 import { render, h } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
 import TogglClient from 'toggl-api';
+import 'babel-polyfill'
 
 function NewPage() {
     const [tokenValue, setTokenValue]=useState('');
-
     const [client, setClient]=useState(new TogglClient())
 
     useEffect(() => {
@@ -12,6 +12,27 @@ function NewPage() {
             setTokenValue(result.token)
             const client=new TogglClient({ apiToken: result.token })
             setClient(client)
+            function getPreviousMonday() {
+                var date=new Date();
+                var day=date.getDay();
+                var prevMonday=new Date();
+                if (date.getDay()===0) {
+                    prevMonday.setDate(date.getDate()-7);
+                } else {
+                    prevMonday.setDate(date.getDate()-(day-1));
+                }
+                prevMonday.setHours(0, 0, 0, 0);
+                return prevMonday.toISOString();
+            }
+
+            console.log('sup')
+            client.getTimeEntries(
+                getPreviousMonday(),
+                new Date().toISOString(),
+                (err, timeEntries) => {
+                    console.log(timeEntries, err)
+                }
+            )
         })
     }, [])
 
