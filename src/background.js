ae3ca4a78af7aa5a1eb52b96a82b678f488cbc24
Rafael Goncalves <capitao.rafa@gmail.com>
2022-03-23 19:39:29 -0300

    Cleanup

--- src/background.js
+++ src/background.js
@@ -4,47 +4,62 @@ import {
     getTime,
     getPreviousMonday
 } from './utils'
-// Where we will expose all the data we retrieve from storage.sync.
-const storageCache = {};
 
+const storageCache = {};
 let client;
-// Asynchronously retrieve data from storage.sync, then cache it.
+
 const initStorageCache = getAllStorageSyncData().then(items => {
-    // Copy the data retrieved from storage into storageCache.
     Object.assign(storageCache, items);
 }).then(() => {
     console.log('Background', storageCache)
     client = new TogglClient({
         apiToken: storageCache.token
     })
+    getWorkspaces()
+    getTimeEntries()
+})
 
+function getAllStorageSyncData() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(null, (items) => {
+            if (chrome.runtime.lastError) {
+                return reject(chrome.runtime.lastError);
+            }
+            resolve(items);
+        });
+    });
+}
 
+function getWorkspaces() {
     client.getWorkspaces((err, workspaces) => {
         if (err) {
             console.log("Error getting workspaces: ", err)
         } else {
-
             chrome.storage.local.set({
                 workspaces: workspaces
             })
-            workspaces.forEach(ws => {
-                console.log('ws', ws)
-                client.getWorkspaceProjects(ws.id, {
-                    active: 'both'
-                }, (err, projects) => {
-                    if (err) {
-                        console.log("Error getting projects from workspace: ", err)
-                    } else {
-                        chrome.storage.local.set({
-                            projects: projects
-                        })
-                    }
-                })
-            });
+            getProjects(workspaces)
         }
-
     })
+}
 
+function getProjects(workspaces) {
+    workspaces.forEach(ws => {
+        client.getWorkspaceProjects(ws.id, {
+            active: 'both'
+        }, (err, projects) => {
+            if (err) {
+                console.log("Error getting projects from workspace: ", err)
+            } else {
+                chrome.storage.local.set({
+                    projects: projects
+                })
+            }
+        })
+    });
+}
+
+function getTimeEntries() {
     client.getTimeEntries(
         getPreviousMonday(),
         new Date().toISOString(),
@@ -64,36 +79,4 @@ const initStorageCache = getAllStorageSyncData().then(items => {
             }
         }
     )
-})
-
-
-// chrome.action.onClicked.addListener(async (tab) => {
-//     try {
-//         await initStorageCache;
-//     } catch (e) {
-//         // Handle error that occurred during storage initialization.
-//     }
-//     // Normal action handler logic.
-// });
-
-
-
-
-// Reads all data out of storage.sync and exposes it via a promise.
-//
-// Note: Once the Storage API gains promise support, this function
-// can be greatly simplified.
-function getAllStorageSyncData() {
-    // Immediately return a promise and start asynchronous work
-    return new Promise((resolve, reject) => {
-        // Asynchronously fetch all data from storage.sync.
-        chrome.storage.local.get(null, (items) => {
-            // Pass any observed errors down the promise chain.
-            if (chrome.runtime.lastError) {
-                return reject(chrome.runtime.lastError);
-            }
-            // Pass the data retrieved from storage down the promise chain.
-            resolve(items);
-        });
-    });
 }
