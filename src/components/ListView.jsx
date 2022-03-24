e36050ccf9f3f4d1e749215ea99e8a0220874282
Rafael Goncalves <capitao.rafa@gmail.com>
2022-03-23 21:57:27 -0300

    Expansion panels

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -1,12 +1,49 @@
-import { render, h } from 'preact';
+import { render, h, Fragment } from 'preact';
 import ListItem from './ListItem.jsx';
+
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import {
+    formatDate
+} from '../utils'
 
 export default function ListView(props) {
     const entries=props.entries? props.entries.reverse():[]
+    let days=[]
 
-    return <div >
-        {entries.map((entry) =>
-            <ListItem entry={entry} />
-        )
-        } </div>
+    entries.forEach((entry) => {
+        let entryDate=formatDate(new Date(entry.start));
+        let day=days.find((d) => d.label==entryDate);
+        if (!day) {
+            days.push({ label: entryDate });
+            day=days.find((d) => d.label==entryDate);
+        }
+        if (!day.entries) day.entries=[];
+        day.entries.push(entry);
+    });
+
+    days.sort();
+
+    return <Fragment >
+        {days.map(day =>
+            <Accordion >
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>{day.label}</Typography>
+                </AccordionSummary>
+                <AccordionDetails style={{ padding: 0 }}>
+                    {day.entries.map((entry) =>
+                        <ListItem entry={entry} />
+                    )}
+                </AccordionDetails>
+            </Accordion>
+        )}
+    </Fragment>
 }
--- src/newtab.jsx
+++ src/newtab.jsx
@@ -6,6 +6,15 @@ import GoalsView from './components/GoalsView.jsx';
 import Box from '@mui/material/Box';
 import css from "../global.css";
 
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+
+const darkTheme=createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+
 function NewPage() {
     const [entries, setEntries]=useState([]);
 
@@ -18,24 +27,22 @@ function NewPage() {
         })
     }, [])
 
-    return <Box className='parentBox' >
-
-        <Box className='childBox'>
-            <h2 className='boxLabel'> Weekly Goals </h2>
-            <GoalsView />
+    return (<ThemeProvider theme={darkTheme}>
+        <Box className='parentBox' >
+            <Box className='childBox'>
+                <h2 className='boxLabel'> Weekly Goals </h2>
+                <GoalsView />
+            </Box>
+            <Box className='childBox'>
+                <h2 className='boxLabel'> Weekly List </h2>
+                <ListView entries={entries} />
+            </Box>
+            <Box className='childBox'>
+                <h2 className='boxLabel'> My Favorites </h2>
+                <FavoritesView />
+            </Box>
         </Box>
-
-        <Box className='childBox'>
-            <h2 className='boxLabel'> Weekly List </h2>
-            <ListView entries={entries} />
-        </Box>
-
-        <Box className='childBox'>
-            <h2 className='boxLabel'> My Favorites </h2>
-            <FavoritesView />
-        </Box>
-
-    </Box>
+    </ThemeProvider>)
 }
 
 render(<NewPage />,
