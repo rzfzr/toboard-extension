d68fd4e8620910d3a6989c64a6ebb36a7269c462
Rafael Goncalves <capitao.rafa@gmail.com>
2022-04-09 18:14:25 -0300

    Better layout

--- src/options.jsx
+++ src/options.jsx
@@ -1,5 +1,16 @@
+import "../global.css";
 import { render, h } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { TextField, Paper, Grid, Button, Box } from '@mui/material';
+
+const darkTheme=createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 function OptionsPage() {
     const [tokenValue, setTokenValue]=useState('');
@@ -11,26 +22,41 @@ function OptionsPage() {
         })
     }, [])
 
-    const onSubmit=(event) => {
+    const saveForm=(event) => {
         event.preventDefault();
         chrome.storage.local.set({
             token: tokenValue,
         }, () => {
-            setStatus("Token was saved !")
+            setStatus("ok")
             setTimeout(() => {
                 setStatus('');
-            }, 750);
+            }, 1*1000);
         });
     }
 
     const onInput=(event) => setTokenValue(event.target.value)
 
-    return <form onSubmit={onSubmit}>
-        <label for="username">Enter API Token:</label>
-        <input type="text" value={tokenValue} onInput={onInput} />
-        <p>{status}</p>
-        <button type="submit">Submit</button>
-    </form>
+    return <ThemeProvider theme={darkTheme}>
+        <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            style={{ height: '80vh' }}
+        >
+            <Paper elevation={10} style={{ width: '30%' }}>
+                <form onSubmit={saveForm} >
+                    <TextField fullWidth value={tokenValue} onInput={onInput} label="API Token" variant="standard" color='primary' />
+                    <p>
+                        {status==='ok'&&"Form was saved!"}
+                    </p>
+                    <Button variant="text" type="submit" color={status==='ok'? 'success':'primary'}>Submit</Button>
+                    <Button variant="text" color="error" disabled>Reset All</Button>
+
+                </form>
+            </Paper>
+        </Grid>
+    </ThemeProvider>
 }
 
 render(<OptionsPage />,
