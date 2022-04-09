c0affa04f6347234f316949c945023a34cde772c
Rafael Goncalves <capitao.rafa@gmail.com>
2022-04-09 11:58:26 -0300

    Fixed high cpu usage from watcher

--- webpack.config.js
+++ webpack.config.js
@@ -66,8 +66,8 @@ module.exports = {
     ],
     watch: true,
     watchOptions: {
-        aggregateTimeout: 200,
-        poll: 1000,
+        // aggregateTimeout: 200,
+        // poll: 1000,
         ignored: './node_modules/',
     },
     resolve: {
