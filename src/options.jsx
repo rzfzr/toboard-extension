7981fa3de0934b0e39d19a2f63eea1dc205db96c
Rafael Goncalves <capitao.rafa@gmail.com>
2022-04-11 11:45:46 -0300

    Added mock options

--- src/options.jsx
+++ src/options.jsx
@@ -4,7 +4,29 @@ import { useState, useEffect } from 'preact/hooks';
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import { TextField, Paper, Grid, Button, Box } from '@mui/material';
+import {
+    TextField,
+    Paper,
+    Card,
+    CardActions,
+    CardContent,
+    Grid,
+    Button,
+    ButtonGroup,
+    Box,
+    Typography,
+    FormControl,
+    FormControlLabel,
+    InputLabel,
+    Switch,
+    Select,
+    MenuItem,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails
+} from '@mui/material';
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const darkTheme=createTheme({
     palette: {
@@ -15,6 +37,14 @@ const darkTheme=createTheme({
 function OptionsPage() {
     const [tokenValue, setTokenValue]=useState('');
     const [status, setStatus]=useState('');
+
+    const [expansions, setExpansions]=useState([true, false, false]);
+
+    const handleExpansion=sel => (event) => {
+        let temp=[...expansions]
+        temp[sel]=!temp[sel]
+        setExpansions(temp);
+    };
 
     useEffect(() => {
         chrome.storage.local.get(['token'], (result) => {
@@ -44,17 +74,87 @@ function OptionsPage() {
             alignItems="center"
             style={{ height: '80vh' }}
         >
-            <Paper elevation={10} style={{ width: '30%' }}>
+            <Card elevation={10} style={{ width: '30%' }}>
                 <form onSubmit={saveForm} >
-                    <TextField fullWidth value={tokenValue} onInput={onInput} label="API Token" variant="standard" color='primary' />
-                    <p>
-                        {status==='ok'&&"Form was saved!"}
-                    </p>
-                    <Button variant="text" type="submit" color={status==='ok'? 'success':'primary'}>Submit</Button>
-                    <Button variant="text" color="error" disabled>Reset All</Button>
+                    <CardContent>
+                        <Accordion expanded={expansions[0]} onChange={handleExpansion(0)}>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography>Main</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <TextField fullWidth value={tokenValue} onInput={onInput} label="API Token" variant="standard" color='primary' />
+                                <FormControl fullWidth>
+                                    <FormControlLabel disabled control={<Switch defaultChecked />} label="Show Weekly Goals" />
+                                </FormControl>
+                                <FormControl fullWidth>
+                                    <FormControlLabel disabled control={<Switch defaultChecked />} label="Show Weekly List" />
+                                </FormControl>
+                                <FormControl fullWidth>
+                                    <FormControlLabel disabled control={<Switch defaultChecked />} label="Show My Favorites" />
+                                </FormControl>
+                                <FormControl fullWidth>
+                                    <FormControlLabel disabled control={<Switch />} label="Show Timeline" />
+                                </FormControl>
+                                <FormControl fullWidth>
+                                    <FormControlLabel disabled control={<Switch />} label="Show Calendar" />
+                                </FormControl>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion expanded={expansions[1]} onChange={handleExpansion(1)}>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel2a-content"
+                                id="panel2a-header"
+                            >
+                                <Typography>Weekly Goals</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <FormControl fullWidth>
+                                    <FormControlLabel disabled control={<Switch />} label="Rolling Start" />
+                                </FormControl>
 
+                                <FormControl fullWidth>
+                                    <InputLabel >Week starts on</InputLabel>
+                                    <Select disabled value='monday'>
+                                        <MenuItem value={'monday'}>Monday</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel2a-content"
+                                id="panel2a-header"
+                                onChange={handleExpansion(2)}
+                            >
+                                <Typography>Miscellaneous</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <FormControl fullWidth>
+                                    <FormControlLabel disabled control={<Switch />} label="Show Bookmarks" />
+                                </FormControl>
+                                <FormControl fullWidth>
+                                    <FormControlLabel disabled control={<Switch />} label="Show GitHub link" />
+                                </FormControl>
+                                <FormControl fullWidth>
+                                    <FormControlLabel disabled control={<Switch />} label="Enable Sounds" />
+                                </FormControl>
+                            </AccordionDetails>
+                        </Accordion>
+                    </CardContent>
+                    <CardActions>
+                        <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group">
+                            <Button type="submit" color={status==='ok'? 'success':'primary'}>Submit</Button>
+                            <Button color="error" disabled>Reset All</Button>
+                        </ButtonGroup>
+                    </CardActions>
                 </form>
-            </Paper>
+            </Card>
         </Grid>
     </ThemeProvider>
 }
