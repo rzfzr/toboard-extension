b70430438eb2a7927b2763e37262a76a881a27f7
rzfzr <capitao.rafa@gmail.com>
2024-04-21 17:11:12 -0300

    Bootstrapping NewPage

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+function bootstrap(Component: React.ElementType) {
+  //@ts-ignore
+  console.log('bootstrapping', Component.displayName)
+  const root = ReactDOM.createRoot(document.getElementById('root')!);
+  root.render(
+    <React.StrictMode>
+      <Component />
+    </React.StrictMode>
+  );
+}
+
+export default bootstrap;
