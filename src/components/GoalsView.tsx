d46e71e3ac9ec0a2fa242c6da5241db6b1e07976
rzfzr <capitao.rafa@gmail.com>
2024-04-29 19:06:19 -0300

    Fixed goals state

--- src/components/GoalsView.tsx
+++ src/components/GoalsView.tsx
@@ -5,57 +5,46 @@ import NewGoal from './NewGoal.jsx';
 
 import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
-import { Goal } from '../toboard.js';
-
-function updateGoal(goal: Goal, entries: any) {
-    const goalEntries = goal.description == '' ?
-        entries.filter((entry: any) => (entry.pid === goal.project.id)) :
-        entries.filter((entry: any) => (entry.pid === goal.project.id && entry.description === goal.description))
-
-    goal.isRunning = !!goalEntries.find((e: any) => e.duration < 0)
-    goal.duration = goalEntries.reduce((p: any, c: any) => {
-        if (c.duration < 0) {
-            let n = Date.now() / 1000
-            return p + n + c.duration
-        }
-        return p + c.duration
-    }, 0)
-    return goal
-}
+import { Goal, Entry } from '../toboard.js';
 
 export default function GoalsView(props: any) {
     const [goals, setGoals] = useState([] as any[]);
     const entries = props.entries
     const [isEditing, setIsEditing] = useState(false);
 
-    useEffect(() => {
-        chrome.storage.sync.get(['goals'], (result: any) => {
-            if (!result.goals) return
-            result.goals.forEach((goal: any) => {
-                goal = updateGoal(goal, entries)
-            });
-            setGoals(result.goals || [])
-        })
-    }, [entries])
-
-    useEffect(() => {
+    const setAndSaveGoals = (goals: Goal[]) => {
+        setGoals(goals || [])
         chrome.storage.sync.set({
             goals: goals,
         })
-    }, [goals])
+    }
+
+    useEffect(() => {
+        console.log('-> entries changed, updating goals')
+        chrome.storage.sync.get(['goals'], (result: any) => {
+            if (!result.goals || result.goals.length === 0) {
+                return
+            }
+
+            result.goals.forEach((goal: Goal) => {
+                goal = getUpdatedGoal(goal, entries)
+            });
+            setAndSaveGoals(result.goals)
+        })
+    }, [entries])
 
     function deleteGoal(goal: any) {
-        setGoals(goals.filter((f: any) => !(f.description === goal.description && f.project.name === goal.project.name)))
+        setAndSaveGoals(goals.filter((f: any) => !(f.description === goal.description && f.project.name === goal.project.name)))
     }
     function addGoal(description: any, project: any, target: any) {
-        const goal = updateGoal({
+        const goal = getUpdatedGoal({
             id: Date.now(),
             description: description || '',
             project: project.name ? project : { name: project },
             target: target,
             duration: 0
         }, entries)
-        setGoals([...goals, goal])
+        setAndSaveGoals([...goals, goal])
     }
     return <div >
         {goals.map((goal) =>
@@ -66,4 +55,20 @@ export default function GoalsView(props: any) {
         </IconButton>
 
         {isEditing && <NewGoal add={addGoal} />}</div>
+}
+
+function getUpdatedGoal(goal: Goal, entries: Entry[]) {
+    const goalEntries = goal.description == '' ?
+        entries.filter((entry: Entry) => (entry.pid === goal.project.id)) :
+        entries.filter((entry: Entry) => (entry.pid === goal.project.id && entry.description === goal.description))
+
+    goal.isRunning = !!goalEntries.find((e: any) => e.duration < 0)
+    goal.duration = goalEntries.reduce((p: any, c: any) => {
+        if (c.duration < 0) {
+            let n = Date.now() / 1000
+            return p + n + c.duration
+        }
+        return p + c.duration
+    }, 0)
+    return goal
 }
