f59ea6576ba1ade9c791ee72183da6f0f929c44b
rzfzr <capitao.rafa@gmail.com>
2024-05-04 13:40:24 -0300

    Extracting card text

---
+++ src/components/CardText.tsx
@@ -0,0 +1,24 @@
+import { Box, CardContent, Typography } from "@mui/material"
+import { Project } from "../toboard"
+
+export default function CardText(props: { description: string | null | undefined, project: Project | null | undefined }) {
+    return (
+        <Box className="left-0 right-0 flex flex-col w-3/4 text-xl text-white" >
+            <CardContent sx={{
+                className: 'flex flex-col w-3/4 text-xl text-white ',
+            }}>
+                <Typography
+                    className='relative z-50'
+                    variant="subtitle1">
+                    {props.description != '' ? props.description : props.project?.name}
+                </Typography>
+                <Typography
+                    className='relative z-50'
+                    variant="subtitle2"
+                >
+                    {props.description != '' ? props.project?.name : ''}
+                </Typography>
+            </CardContent>
+        </Box>
+    )
+}
