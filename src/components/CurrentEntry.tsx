ed921472da9dc12e917d011563bacbcd221f19cb
rzfzr <capitao.rafa@gmail.com>
2024-05-05 13:27:41 -0300

    Cleanup

--- src/components/CurrentEntry.tsx
+++ src/components/CurrentEntry.tsx
@@ -1,15 +1,6 @@
 import EntryComponent from './EntryComponent.js'
 
-import Accordion from '@mui/material/Accordion'
-import AccordionSummary from '@mui/material/AccordionSummary'
-import AccordionDetails from '@mui/material/AccordionDetails'
-import Typography from '@mui/material/Typography'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import useStore from '../useStore.js'
-
-import {
-    formatDate
-} from '../utils.js'
 
 export default function CurrentEntry() {
     const entry = useStore((state) => state.entries[0])
