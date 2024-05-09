3f9b300724eb4c43cd436362a68274f553fb87d2
rzfzr <capitao.rafa@gmail.com>
2024-05-09 17:29:28 -0300

    Removing more unneded polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,30 @@ import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.config'
 
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+//Polyfills needed for dev = stream, https, http
 
 export default defineConfig({
   plugins: [react(),
   crx({ manifest }),
   nodePolyfills({
-    include: ['stream', 'https', 'http', 'util', 'url', 'buffer'],
+    include: [
+      'stream',
+      'https',
+      'http',
+      // 'util',
+      // 'url',
+      // 'buffer'
+    ],
     overrides: {
       stream: 'stream-browserify',
       https: "https-browserify",
       http: "stream-http",
-      util: "util/",
-      url: "url/",
-      buffer: "buffer/",
+      // util: "util/",
+      // url: "url/",
+      // buffer: "buffer/",
     },
   })],
   build: {
-    minify: true,
+    minify: false,
   },
 })
