9fb1e28c70d14b8bf27f017fc0a138de43a07532
rzfzr <capitao.rafa@gmail.com>
2024-06-04 13:46:46 -0300

    Removing buffer polyfil, minifying on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { crx } from "@crxjs/vite-plugin";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { crx } from "@crxjs/vite-plugin"
 //@ts-ignore
-import manifest from "./manifest.config";
+import manifest from "./manifest.config"
 
-import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { nodePolyfills } from "vite-plugin-node-polyfills"
 
 export default defineConfig({
   plugins: [
     crx({ manifest }),
     react(),
     nodePolyfills({
-      include: ["stream", "https", "http", "buffer"],
+      include: ["stream", "https", "http"],
       globals: {
         process: true,
       },
     }),
   ],
   build: {
-    minify: false,
+    minify: true,
   },
-});
+})
